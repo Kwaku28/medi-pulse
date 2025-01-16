afa4f252485593aba509eac714e42e14be932cca
Kwaku28 <rekenburg777@gmail.com>
2025-01-16 08:02:15 +0000

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -30,12 +30,8 @@ export const IdentificationTypes = [
   "Birth Certificate",
   "Driver's License",
   "Medical Insurance Card/Policy",
-  "Military ID Card",
   "National Identity Card",
   "Passport",
-  "Resident Alien Card (Green Card)",
-  "Social Security Card",
-  "State ID Card",
   "Student ID Card",
   "Voter ID Card",
 ];
