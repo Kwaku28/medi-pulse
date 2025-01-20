7211426f836e02604c10f0e6c6652bb7cc456eef
Kwaku28 <rekenburg777@gmail.com>
2025-01-20 13:15:09 +0000

    Update homepage ui to display admin otp popup window

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const resolvedSearchParams = await searchParams;
+  const isAdmin = resolvedSearchParams?.admin === 'true';
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
