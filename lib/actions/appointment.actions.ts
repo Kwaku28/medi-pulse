3db60578e66bd1a48a0b707a376f4d0b19081bb3
Kwaku28 <rekenburg777@gmail.com>
2025-01-21 02:54:04 +0000

    Implement function to fetch the list of appointments

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,9 +1,10 @@
 "use server"
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams
@@ -39,3 +40,42 @@ export const getAppointment = async (appointmentId: string) => {
     );
   }
 };
+
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointment = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc('$createdAt')]
+    );
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0
+    }
+
+    const counts = (appointment.documents as Appointment[]).reduce((acc, appointment) => {
+      if (appointment.status === "scheduled") {
+        acc.scheduledCount += 1;
+      } else if (appointment.status === "pending") {
+        acc.pendingCount += 1
+      } else if (appointment.status === "cancelled") {
+        acc.cancelledCount += 1
+      }
+
+      return acc;
+    }, initialCounts)
+
+    const data = {
+      totalCount: appointment.total,
+      ...counts,
+      document: appointment.documents
+    }
+
+    return parseStringify(data);
+
+  } catch (error) {
+    console.log(error)
+  }
+}
